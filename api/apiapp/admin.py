438f902a85455972a795588eae53ba4cb7310049
Dmitry <dz5011@yandex.ru>
2023-03-06 17:33:56 +0300

    models mini course and student register in admin panel

--- api/apiapp/admin.py
+++ api/apiapp/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from .models import TgUser
+from .models import TgUser, MiniCours, Student
 
 
 @admin.register(TgUser)
 class TgUserAdmin(admin.ModelAdmin):
     list_display = ('tg_id', 'first_name', 'tg_username')
     list_display_links = ('tg_id', 'first_name', 'tg_username')
+
+
+@admin.register(MiniCours)
+class MiniCoursAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'start_time')
+    list_display_links = ('title',)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'is_presence')
+    list_display_links = ('first_name', 'last_name')
