05ee5454b48f278cfc2fb687d09f512c643628f8
Dmitry <dz5011@yandex.ru>
2023-03-06 17:36:16 +0300

    views ofr mini course and student created

--- api/apiapp/views.py
+++ api/apiapp/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, ListAPIView
 
-from .custom_mixins import TgUserAPIMixin
+from .custom_mixins import TgUserAPIMixin, MiniCourseMixin, StudentMixin
+from .serializers import StudentSerializer
+from .models import Student
 
 
 class CreateTgUser(TgUserAPIMixin, CreateAPIView):
@@ -13,3 +15,18 @@ class ListTgUser(TgUserAPIMixin, ListAPIView):
 
 class ReadOrUpdateTgUser(TgUserAPIMixin, RetrieveUpdateAPIView):
     lookup_field = 'tg_id'
+
+
+class ListMiniCousres(MiniCourseMixin, ListAPIView):
+    pass
+
+
+class ListStudents(ListAPIView):
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        return Student.objects.filter(course_id=self.kwargs.get('pk'))
+
+
+class ReadOrUpdateStudent(StudentMixin, RetrieveUpdateAPIView):
+    pass
