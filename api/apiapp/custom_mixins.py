980813913325682a30bdd059969a0bb07b81b135
Dmitry <dz5011@yandex.ru>
2023-03-06 17:33:13 +0300

    mixins for mini course and student created

--- api/apiapp/custom_mixins.py
+++ api/apiapp/custom_mixins.py
@@ -1,8 +1,18 @@
-from .serializers import TgUserSerializer
-from .models import TgUser
+from .serializers import TgUserSerializer, MiniCourseSerializer, StudentSerializer
+from .models import TgUser, MiniCours, Student
 
 
 class TgUserAPIMixin:
     serializer_class = TgUserSerializer
     queryset = TgUser.objects.all()
+
+
+class MiniCourseMixin:
+    serializer_class = MiniCourseSerializer
+    queryset = MiniCours.objects.all()
+
+
+class StudentMixin:
+    serializer_class = StudentSerializer
+    queryset = Student.objects.all()
     
