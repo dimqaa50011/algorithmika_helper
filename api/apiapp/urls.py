6191e582766e8da6f5a05da7b7ee0cf4ddaf165a
Dmitry <dz5011@yandex.ru>
2023-03-06 17:37:16 +0300

    url for mini course and student created

--- api/apiapp/urls.py
+++ api/apiapp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('user/', api_views.CreateTgUser.as_view()),
     path('user/<int:tg_id>/', api_views.ReadOrUpdateTgUser.as_view()),
     path('user/list/', api_views.ListTgUser.as_view()),
+    path('course/list/', api_views.ListMiniCousres.as_view()),
+    path('student/list/<int:pk>', api_views.ListStudents.as_view()),
+    path('student/<int:pk>', api_views.ReadOrUpdateStudent.as_view()),
 ]
